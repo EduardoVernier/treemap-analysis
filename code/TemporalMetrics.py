20382db85ad96c3ceb4395f72b4a7bb4797b98e3
Eduardo Vernier <efvernier@gmail.com>
2017-11-16 23:50:02 -0200

    Implement delta_data and q_ratio.

--- code/TemporalMetrics.py
+++ code/TemporalMetrics.py
@@ -3,8 +3,11 @@ import math
 
 
 def q_ratio(df1, df2):
-
-    pass
+    # Create a df with columns delta_vis and delta_data
+    df = delta_vis(df1, df2).copy()
+    df = pd.merge(df, delta_data_by_area(df1, df2))
+    df['q_ratio'] = (1 - df['delta_vis']) / (1 - df['delta_data'])
+    return df
 
 
 def q_mod(df1, df2):
@@ -14,18 +17,27 @@ def q_mod(df1, df2):
 def delta_vis(df1, df2):
     base_width = (df1['x'] + df1['w']).max()
     base_height = (df1['y'] + df1['h']).max()
+    # Normalize by 4 * hypotenuse
     norm = 4 * math.sqrt(base_width ** 2 + base_height ** 2)
 
-    df1.columns = ['id', 'x1', 'y1', 'w1', 'h1']
-    df2.columns = ['id', 'x2', 'y2', 'w2', 'h2']
     df = pd.merge(df1, df2, on='id', how='outer')
+    df.columns = ['id', 'x1', 'y1', 'w1', 'h1', 'x2', 'y2', 'w2', 'h2']
     dv = df[['id']].copy()
     dv['delta_vis'] = df.apply(lambda r: corner_travel(*list(r)) / norm, axis=1)
     return dv
 
 
 def delta_data_by_area(df1, df2):
-    pass
+    base_width = (df1['x'] + df1['w']).max()
+    base_height = (df1['y'] + df1['h']).max()
+    # Normalize by base area
+    norm = base_height * base_width
+
+    df = pd.merge(df1, df2, on='id', how='outer')
+    df.columns = ['id', 'x1', 'y1', 'w1', 'h1', 'x2', 'y2', 'w2', 'h2']
+    dd = df[['id']].copy()
+    dd['delta_data'] = df.apply(lambda r: area_change(*list(r)) / norm, axis=1)
+    return dd
 
 
 # If the precision of rectangles is good enough, this shouldn't be necessary
@@ -49,3 +61,13 @@ def corner_travel(*args):
             + point_distance(x1 + w1, y1, x2 + w2, y2)   \
             + point_distance(x1, y1 + h1, x2, y2 + h2)   \
             + point_distance(x1 + w1, y1 + h1, x2 + w2, y2 + h2)
+
+
+def area_change(*args):
+    _, x1, y1, w1, h1, x2, y2, w2, h2 = args
+    if math.isnan(w1):
+        return w2 * h2
+    elif math.isnan(w2):
+        return w1 * h1
+    else:
+        return abs(w1 * h1 - w2 * h2)
--- code/main.py
+++ code/main.py
@@ -1,8 +1,9 @@
 import Parser
 import SpatialMetrics
 import TemporalMetrics
+import pandas as pd
 
 # df = Parser.parse_dataset('exo')
 sqr_exo = Parser.parse_rectangles('sqr', 'exo')
 # SpatialMetrics.q_diag(sqr_exo[0])
-TemporalMetrics.delta_vis(sqr_exo[0], sqr_exo[1])
+TemporalMetrics.q_ratio(sqr_exo[0], sqr_exo[1])

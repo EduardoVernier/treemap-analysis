db55a1dfde336cfb3f7985353299fff07efa8b24
Eduardo Vernier <efvernier@gmail.com>
2017-11-21 00:17:10 -0200

    Spatial boxplots.

--- code/SpatialMetrics.py
+++ code/SpatialMetrics.py
@@ -15,31 +15,27 @@ def aggregated_ar(technique_id, dataset_id):
         df_ar[column] = df_ar.mean(axis=1)
         df_mean = pd.concat([df_mean, df_ar[column]], axis=1, ignore_index=True)
     # Return a dataframe where each column is a revision and each row a cell
-    return df_mean
+    return df_mean.T
 
 
 def q_ar(df):
-    ar = df[['id']].copy()
-    ar['q_ar'] = df[['w', 'h']].min(axis=1) / df[['w', 'h']].max(axis=1)
-    return ar
+    df['q_ar'] = df[['w', 'h']].min(axis=1) / df[['w', 'h']].max(axis=1)
+    return df[['id', 'q_ar']]
 
 
 def q_weighted_ar(df):
     total_area = (df['x'] + df['w']).max() * (df['y'] + df['h']).max()
 
-    weighted_ar = df[['id']].copy()
-    weighted_ar['q_w_ar'] = (df[['w', 'h']].min(axis=1) / df[['w', 'h']].max(axis=1)) \
-                            * ((df['w'] * df['h']) / total_area)
-    return weighted_ar
+    df['q_w_ar'] = (df[['w', 'h']].min(axis=1) / df[['w', 'h']].max(axis=1)) * ((df['w'] * df['h']) / total_area)
+    return df[['id', 'q_w_ar']]
 
 
 def q_diag(df):
     # Find out max(width, height) of the base rectangle -- should be 1000
     max_dim = max((df['x'] + df['w']).max(), (df['y'] + df['h']).max())
 
-    diag = df[['id']].copy()
-    diag['q_diag'] = 1 - abs(df['w'] - df['h']) / max_dim
-    return diag
+    df['q_diag'] = 1 - abs(df['w'] - df['h']) / max_dim
+    return df[['id', 'q_diag']]
 
 
 def q_weighted_diag(df):
@@ -47,8 +43,5 @@ def q_weighted_diag(df):
     max_dim = max((df['x'] + df['w']).max(), (df['y'] + df['h']).max())
     total_area = (df['x'] + df['w']).max() * (df['y'] + df['h']).max()
 
-
-    weighted_diag = df[['id']].copy()
-    weighted_diag['q_w_diag'] = (1 - abs(df['w'] - df['h']) / max_dim) \
-                                 * ((df['w'] * df['h']) / total_area)
-    return weighted_diag
+    df['q_w_diag'] = (1 - abs(df['w'] - df['h']) / max_dim) * ((df['w'] * df['h']) / total_area)
+    return df[['id', 'q_w_diag']]

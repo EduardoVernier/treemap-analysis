5e6666dadff9ef1ef681e9b15f0d2e56207ba6b5
Eduardo Vernier <efvernier@gmail.com>
2017-11-30 15:05:28 -0200

    Max.

--- code/TemporalMetrics.py
+++ code/TemporalMetrics.py
@@ -161,3 +161,141 @@ def relative_weight_mean(*args):
         return ((w1 * h1) / area1) / 2
     else:
         return ((w1 * h1) / area1 + (w2 * h2) / area2) / 2
+
+
+#####################################################
+# Relative Position Change Metric from 'Stable Treemaps via Local Moves' 2017
+#  - Contains a list of the rectangles in the current and in the new iteration. Only those rectangles present in both are there
+#    a object in the list should contain "x1","x2","y1","y2" values
+def relative_position_change_wrapper(df1, df2):
+    df = pd.merge(df1, df2, on='id', how='inner')  # Retain only rows in both sets
+    df.columns = ['id', 'x1', 'y1', 'w1', 'h1', 'x2', 'y2', 'w2', 'h2']
+    l1 = df[['x1', 'y1', 'w1', 'h1']]
+    l1['w1'] = df['x1'] + df['w1']
+    l1['h1'] = df['y1'] + df['h1']
+    l1.columns = ['x1', 'y1', 'x2', 'y2']
+
+    l2 = df[['x2', 'y2', 'w2', 'h2']]
+    l2['w2'] = df['x2'] + df['w2']
+    l2['h2'] = df['y2'] + df['h2']
+    l2.columns = ['x1', 'y1', 'x2', 'y2']
+
+    score = getRelativeScore(l1, l2)
+    return score
+
+
+def getRelativeScore(listR1, listR2):
+    stability = 0
+
+    for i, r1 in listR1.iterrows():
+        for j, r2 in listR2.iterrows():
+            if (i != j):
+                oldPercentage = getRelativePositions(r1['x1'], r1['x2'], r1['y1'], r1['y2'], r2['x1'], r2['x2'],
+                                                     r2['y1'], r2['y2'])
+                newPercentage = getRelativePositions(r1['x1'], r1['x2'], r1['y1'], r1['y2'], r2['x1'], r2['x2'],
+                                                     r2['y1'], r2['y2'])
+                itemStability = getQuadrantStability(oldPercentage, newPercentage)
+                stability += itemStability
+
+    items = len(listR1)
+    stability = stability / (pow(items, 2) - items)
+    return stability
+
+
+def getQuadrantStability(percentagesOld, percentagesNew):
+    stability = 1
+    for i in range(0, 8):
+        oldPercentage = percentagesOld[i]
+        newPercentage = percentagesNew[i]
+        stability -= abs(oldPercentage - newPercentage) / 2
+    return stability
+
+
+# gets the relative positions per quadrant from r1 to r2
+def getRelativePositions(x11, x12, y11, y12, x21, x22, y21, y22):
+    E = 0
+    NE = 0
+    N = 0
+    NW = 0
+    W = 0
+    SW = 0
+    S = 0
+    SE = 0
+
+    if (x21 >= x12):
+        # Strictly east
+        if (y21 < y11):
+            # at least partially in NE
+            # get the percentage that r2 is in NE
+            NE = (y11 - y21) / (y22 - y21)
+            NE = min(1, NE)
+
+        if (y22 > y12):
+            # at least partiall in SE
+            SE = (y22 - y12) / (y22 - y21)
+            SE = min(1, SE)
+
+            # remainder is in east
+            E = 1 - NE - SE
+    elif (x22 <= x11):
+        # strictly west
+        if (y21 < y11):
+            # at least partially in NW
+            # get the percentage that r2 is in NW
+            NW = (y11 - y21) / (y22 - y21)
+            NW = min(1, NW)
+
+        if (y22 > y12):
+            # at least partiall in SW
+            SW = (y22 - y12) / (y22 - y21)
+            SW = min(1, SW)
+
+            # remainder is in west
+            W = 1 - NW - SW
+    elif (y22 <= y11):
+        # strictly North
+        if (x21 < x11):
+            # at least partially in NW
+            # get the percentage that r2 is in NW
+            NW = (x11 - x21) / (x22 - x21)
+            NW = min(1, NW)
+
+        if (x22 > x12):
+            # at least partiall in SW
+            NE = (x22 - x12) / (x22 - x21)
+            NE = min(1, NE)
+
+            # remainder is in west
+            N = 1 - NW - NE
+    else:
+        # strictly south
+        if (x21 < x11):
+            # at least partially in SW
+            # get the percentage that r2 is in NW
+            SW = (x11 - x21) / (x22 - x21)
+            SW = min(1, SW)
+
+        if (x22 > x12):
+            # at least partiall in SE
+            SE = (x22 - x12) / (x22 - x21)
+            SE = min(1, SE)
+
+            # remainder is in west
+            S = 1 - SW - SE
+
+    quadrant = []
+    quadrant.append(E)
+    quadrant.append(NE)
+    quadrant.append(N)
+    quadrant.append(NW)
+    quadrant.append(W)
+    quadrant.append(SW)
+    quadrant.append(S)
+    quadrant.append(SE)
+    return quadrant
+
+# test code
+# array =  getRelativePositions(0,10,10,20,-10,0,0,30)
+
+# print (array)
+
--- code/main.py
+++ code/main.py
@@ -9,12 +9,12 @@ import pandas as pd
 
 #SpatialMetrics.aggregated_ar('git', 'exo')
 
-#history = Parser.parse_rectangles('sqr', 'exo')
-#TemporalMetrics.q_weighted_ratio(history[0], history[1])
+history = Parser.parse_rectangles('sqr', 'exo')
+TemporalMetrics.relative_position_change_wrapper(history[1], history[2])
 
-import timeit
-start_time = timeit.default_timer()
-TemporalMetrics.aggregated_stab('sqr', 'exo')
-elapsed = timeit.default_timer() - start_time
-print(elapsed)
+# import timeit
+# start_time = timeit.default_timer()
+# TemporalMetrics.aggregated_stab('sqr', 'exo')
+# elapsed = timeit.default_timer() - start_time
+# print(elapsed)
 
